c2e7c7b96c38da0acf5f05d254e5590c09caa646
Andres Munoz <andru255@gmail.com>
2023-02-16 22:14:15 -0500

    These changes are related to the following:
    - Updating the proper setup locally with python3
    
    - Replacing the MapBox library with the latest release of Leaflet

--- index.js
+++ index.js
@@ -2,122 +2,136 @@
 var txtLat = document.getElementById("txtLatitude");
 var txtLon = document.getElementById("txtLongitude");
 
-// reset 
-var lnkReset = document.getElementById("lnkReset"); 
+// reset
+var lnkReset = document.getElementById("lnkReset");
 
 // get radios
 var radioModes = document.getElementsByName("radioModes[]");
 
 // get mode by browser
-var lnkGetPositionByBrowser = document.getElementById("lnkGetPositionbyBrowser");
+var lnkGetPositionByBrowser = document.getElementById(
+  "lnkGetPositionbyBrowser"
+);
 
 // message container
 var containerMessage = document.getElementById("messages");
 
 // setup mapbox
-var defaultCoord = [40, -74.50];
-L.mapbox.accessToken = "pk.eyJ1IjoiYW5kcnUyNTUiLCJhIjoiY2pwdzR0ZGRiMHlvOTQ4bnR2OG9lbTNhNSJ9.isi3uGKrTkISJMlnS2T1bw";
-var map = L.mapbox.map("map", "mapbox.streets");
-var marker = new L.Marker( new L.LatLng(defaultCoord[0], defaultCoord[1]), {
-    draggable: true
+var defaultCoord = [40, -74.5];
+var map = L.map("map");
+var marker = new L.Marker(new L.LatLng(defaultCoord[0], defaultCoord[1]), {
+  draggable: true,
 });
 
+// just for the proper copyright that Leaflet indicates
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
 function mapBoxSetup() {
-    // initial centering
-    map.setView(defaultCoord, 9);
-    // adding a marker
-    marker.setLatLng(defaultCoord).addTo(map);
+  // initial centering
+  map.setView(defaultCoord, 9);
+  // adding a marker
+  marker.setLatLng(defaultCoord).addTo(map);
 }
 
 function coordinatesSetup(coordinate) {
-    // update textfields
-    txtLat.value = coordinate[0]; // latitude
-    txtLon.value = coordinate[1]; // longitude
+  // update textfields
+  txtLat.value = coordinate[0]; // latitude
+  txtLon.value = coordinate[1]; // longitude
 }
 
 // Events
 function makeReset(evt) {
-    coordinatesSetup(defaultCoord);
-    marker.setLatLng(defaultCoord);
-    map.setView(defaultCoord, 9);
-    evt.preventDefault();
+  coordinatesSetup(defaultCoord);
+  marker.setLatLng(defaultCoord);
+  map.setView(defaultCoord, 9);
+  evt.preventDefault();
 }
 
 function onMarkerDragEnd() {
-    var lngLat = marker.getLatLng();
-    coordinatesSetup([lngLat.lat, lngLat.lng]);
+  var lngLat = marker.getLatLng();
+  coordinatesSetup([lngLat.lat, lngLat.lng]);
 }
 
 function makeChangeMode() {
-    txtLat.readOnly = (this.id == "drag") ? true : false;
-    txtLon.readOnly = (this.id == "drag") ? true : false;
+  txtLat.readOnly = this.id == "drag" ? true : false;
+  txtLon.readOnly = this.id == "drag" ? true : false;
 
-    if (!txtLat.readOnly || !txtLon.readOnly) {
-        marker.dragging.disable();
-        return;
-    }
+  if (!txtLat.readOnly || !txtLon.readOnly) {
+    marker.dragging.disable();
+    return;
+  }
 
-    marker.dragging.enable();
+  marker.dragging.enable();
 }
 
 function makeUpdatePositionByTextEditing() {
-    if (this.readOnly) {
-        return;
-    }
-    var coordinate = [txtLat.value, txtLon.value];
-    map.setView(coordinate);
-    marker.setLatLng(coordinate);
+  if (this.readOnly) {
+    return;
+  }
+  var coordinate = [txtLat.value, txtLon.value];
+  map.setView(coordinate);
+  marker.setLatLng(coordinate);
 }
 
 function makeUpdatePositionByBrowser(evt) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var coords = position.coords;
-        var latitude = coords.latitude;
-        var longitude = coords.longitude;
-        var coord = [latitude, longitude];
-        coordinatesSetup(coord);
-        marker.setLatLng(coord);
-        map.setView(coord);
-    }, function() {
-        showMessage("Please enable your geolocation feature");
-    });
-    evt.preventDefault();
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      var coords = position.coords;
+      var latitude = coords.latitude;
+      var longitude = coords.longitude;
+      var coord = [latitude, longitude];
+      coordinatesSetup(coord);
+      marker.setLatLng(coord);
+      map.setView(coord);
+    },
+    function () {
+      showMessage("Please enable your geolocation feature");
+    }
+  );
+  evt.preventDefault();
 }
 
 function showMessage(text, timeout) {
-    var validTimeout = timeout || 4;
-    var childElement = document.createElement("div");
-    childElement.className = "message";
-    childElement.innerHTML = ["<p>", text ,"</p>"].join(" ");
-    containerMessage.appendChild(childElement);
-    var timer = setTimeout(() => {
-        childElement.className += " slideup";
-        childElement.remove();
-    }, 1000 * validTimeout);
+  var validTimeout = timeout || 4;
+  var childElement = document.createElement("div");
+  childElement.className = "message";
+  childElement.innerHTML = ["<p>", text, "</p>"].join(" ");
+  containerMessage.appendChild(childElement);
+  var timer = setTimeout(() => {
+    childElement.className += " slideup";
+    childElement.remove();
+  }, 1000 * validTimeout);
 }
 
-window.onload = function() {
-    mapBoxSetup();
-    coordinatesSetup(defaultCoord);
-    marker.on("dragend", onMarkerDragEnd);
+window.onload = function () {
+  mapBoxSetup();
+  coordinatesSetup(defaultCoord);
+  marker.on("dragend", onMarkerDragEnd);
 
-    // for reset
-    lnkReset.addEventListener("click", makeReset);
+  // for reset
+  lnkReset.addEventListener("click", makeReset);
 
-    // switching modes
-    radioModes[0].checked = true;
-    radioModes.forEach(function(radio){
-        radio.addEventListener("click", makeChangeMode);
-    });
+  // switching modes
+  radioModes[0].checked = true;
+  radioModes.forEach(function (radio) {
+    radio.addEventListener("click", makeChangeMode);
+  });
 
-    // for geolocation by browser
-    lnkGetPositionByBrowser.style.display = "none";
-    if ("geolocation" in navigator) {
-        lnkGetPositionByBrowser.style.display = "inline-block";
-        lnkGetPositionByBrowser.addEventListener("click", makeUpdatePositionByBrowser);
-    }
+  // for geolocation by browser
+  lnkGetPositionByBrowser.style.display = "none";
+  if ("geolocation" in navigator) {
+    lnkGetPositionByBrowser.style.display = "inline-block";
+    lnkGetPositionByBrowser.addEventListener(
+      "click",
+      makeUpdatePositionByBrowser
+    );
+  }
 
-    // adding events for inputs
-    txtLat.addEventListener("keyup", makeUpdatePositionByTextEditing);
-    txtLon.addEventListener("keyup", makeUpdatePositionByTextEditing);
+  // adding events for inputs
+  txtLat.addEventListener("keyup", makeUpdatePositionByTextEditing);
+  txtLon.addEventListener("keyup", makeUpdatePositionByTextEditing);
 };
--- server.py
+++ server.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python
 
-import BaseHTTPServer
-import SimpleHTTPServer
+# taken from http://www.piware.de/2011/01/creating-an-https-server-in-python/
+# generate server.pem with the following command:
+#    openssl req -new -x509 -keyout key.pem -out server.pem -days 365 -nodes
+# run as follows:
+#    python3 simple-https-server.py
+# then in your browser, visit:
+#    https://localhost:8000
+
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 import ssl
 
+HOSTNAME = 'localhost'
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = HTTPServer(('localhost', PORT), SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.pem', server_side=True, keyfile="key.pem")
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', PORT), Handler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.pem', server_side=True)
+url = 'https://{hostname}:{port}'.format(hostname=HOSTNAME, port=PORT)
+print ( "serving at port", PORT )
+print ( "go to default browser and put: {url}".format(url=url) )
 
-url = 'https://localhost:{port}'.format(port=PORT)
-print "serving at port", PORT
-print "go to default browser and put: {url}".format(url=url)
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+httpd.server_close()
